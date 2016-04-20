63134999eecd43cb4c2372126ea3f47306e5f42d
Hal Stroemeria <fripfrop@docomo.ne.jp>
2016-04-20 16:53:57 +0900

    capture_testに二値化を追加

--- solver/exe/capture_test.cpp
+++ solver/exe/capture_test.cpp
@@ -1,8 +1,6 @@
 
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/ml/ml.hpp>
+#include <opencv2/opencv.hpp>
 
 
 int main(){
@@ -23,27 +21,48 @@ int main(){
 
 
 	int mode = 0;
+	int thre_mode = 0;
+	int value = 100;
 	while(1)//無限ループ
 	{
-		cv::Mat frame;
-		cap >> frame; // get a new frame from camera
+		cv::Mat origin,frame,gray,thre;
+
+		cap >> origin; // get a new frame from camera
+		resize(origin,frame,cv::Point(), 0.3, 0.3);
 
 		//
 		//取得したフレーム画像に対して，クレースケール変換や2値化などの処理を書き込む．
 		//
 
+		if(thre_mode){
+			
+			cvtColor(frame , gray , CV_BGR2GRAY);
+			cv::threshold(gray, thre, value, 255, cv::THRESH_BINARY);
+			cv::imshow("Thereshold",thre);
+		}
 		cv::imshow("window", frame);//画像を表示．
-
 		int key = cv::waitKey(1);
-		if(key == 113)//qボタンが押されたとき
+		if(key == 'q'-'a' + 97)//qボタンが押されたとき
 		{
 			break;//whileループから抜ける．
 		}
-		else if(key == 115)//sが押されたとき
+		else if(key == 's'-'a' + 97)//sが押されたとき
 		{
 			//フレーム画像を保存する．
 			cv::imwrite("img.png", frame);
+		}else if (key == 'm'-'a' + 97){
+			//二値化
+			thre_mode = 1;
+		}else if(key == 'w'-'a' + 97){
+			//増加
+			value+=5;
+			std::cout << "value:" << value << std::endl;
+		}else if(key == 'e'-'a' + 97){
+			//減少
+			value-=5;
+			std::cout << "value:" << value << std::endl;
 		}
+
 	}
 	cv::destroyAllWindows();
 	return 0;
