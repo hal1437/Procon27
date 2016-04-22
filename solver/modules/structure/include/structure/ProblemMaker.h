1b327c8acc7c89934c3e01164d03090defd8a449
Hal Stroemeria <fripfrop@docomo.ne.jp>
2016-04-22 23:18:25 +0900

    ProblemMakerクラスを作成

---
+++ solver/modules/structure/include/structure/ProblemMaker.h
@@ -0,0 +1,11 @@
+
+#include <structure/Problem.h>
+
+class ProblemMaker{
+
+	//等面積問題作成
+	static Problem MakeEqualAreaProblem();
+	
+	//ランダム問題作成
+	static Problem MakeRandomProblem();
+};
---
+++ solver/modules/structure/src/ProblemMaker.cpp
@@ -0,0 +1,13 @@
+
+#include <structure/ProblemMaker.h>
+
+static Problem ProblemMaker::MakeEqualAreaProblem(){
+	Problem prob;
+	return prob;
+}
+
+static Problem ProblemMaker::MakeRandomProblem(){
+	Problem prob;
+	return prob;
+}
+
