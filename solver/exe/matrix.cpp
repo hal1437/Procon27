99732d4f6fb0f3efde76b1da948aafc0000ee640
Hal Stroemeria <fripfrop@docomo.ne.jp>
2016-08-29 17:05:51 +0900

    isCross関数を追加

--- solver/exe/matrix.cpp
+++ solver/exe/matrix.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <structure/Point.hpp>
 #include <structure/Matrix.hpp>
+#include <structure/Algorithm.h>
 #include <random>
 
 int main(){
@@ -9,8 +10,12 @@ int main(){
 	vMat pos = vMat::ConvertMatrix(Point(1,0));
 	Matrix<3,3> c_mat;
 	
-	Point v1(0, 1);
-	Point v2(0,-1);
+	Point v1(1, 1);
+	Point v2(-1, -1);
+	Point v3( 1,-1);
+	Point v4( 0,0);
+	
+	std::cout << isCrossed(v1,v2,v3,v4) << std::endl;
 
 	std::cout << Point::getAngle2Vec(v1,v2) << std::endl;
 
---
+++ solver/modules/structure/include/structure/Algorithm.h
@@ -0,0 +1,9 @@
+
+
+#pragma once 
+#include<structure/Point.h>
+
+
+//線分が交差しているか
+bool isCrossed(Point p1,Point p2,Point p3,Point p4);
+
