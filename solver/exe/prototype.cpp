821fb5b75ced983691eecf85b850aa3ca111db47
Yuya-Omi <yu1ohmi@gmail.com>
2016-09-25 02:09:39 +0900

    輪郭の取得元を二値化画像二枚のxorに変更. ポーズ機能の追加.

--- solver/exe/prototype.cpp
+++ solver/exe/prototype.cpp
@@ -1,11 +1,14 @@
 
 #include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
+#include <structure/Problem.h>
 #include <util/Console.h>
 #include <string>
 
 
 bool CheckHitKey(int key,char c);
+void cvPointToPoint(cv::Point &cvpoint, Point &point);
 
 
 int main(){
@@ -15,6 +18,7 @@ int main(){
 	std::cin >> device;
 	std::string image_file;
 	cv::VideoCapture cap;
+	Problem problem;
 
 	//カメラデバイスが正常にオープンしたか確認．
 	if(device == -1){
@@ -30,20 +34,24 @@ int main(){
 	}
 
 	int mode = 0;
-	int value = 100;
+	int value1 = 100;
+	int value2 =80;
 	
 	Console::ClearScreen(0);
 	Console::SetCursorPos(0,0);
 	std::cout << "==================================================\n"
-				 "=                 CAPTURE_TEST                   =\n"
+				 "=                   PROTOTYPE                    =\n"
 				 "==================================================\n"
-				 "=  q:exit               [:value up               =\n"
-				 "=  s:save               ]:value down             =\n"
-				 "=  t:threshold                                   =\n"
+				 "=  q:exit               [:value1 up              =\n"
+				 "=  s:save               ]:value1 down            =\n"
+				 "=  t:threshold          @:value2 up              =\n"
+				 "=  p:pause              ::value2 down            =\n"
 				 "==================================================\n"
 				 "=   Mode:                                        =\n"
-				 "=  value:                                        =\n"
+				 "= value1:                                        =\n"
+				 "= value2:                                        =\n"
 				 "=    key:                                        =\n"
+				 "=polygon:                                        =\n"
 				 "==================================================\n"
 				 "=LOG                                              \n"
 				 "                                                  \n"
@@ -53,78 +61,95 @@ int main(){
 				 "                                                  \n"
 				 "==================================================\n";
 
-	cv::Mat origin,frame,gray,thre,tmp;
+	cv::Mat origin,frame,gray,thre1,thre2,thre0;
 	bool nutoral = true;
+	bool pause = false;
 	origin = cv::imread (image_file);
-	while(1)//無限ループ
-	{
+	if (device == -1 && origin.empty()) {
+		std::cout << "file not found !" << std::endl;
+		return -1;
+	}
 
+		while(1)//無限ループ
+	{	
 		//カメラ画像取得、サイズ変換
- 		if(device != -1)cap >> origin;
+ 		if(device != -1 && !pause)cap >> origin;
 		resize(origin,frame,cv::Point(), 1.0, 1.0);
 
         //グレースケール
 		cvtColor(frame, gray, CV_BGR2GRAY);
 		//二値化
 		if (mode == 0) {
-			cv::threshold(gray, thre, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+			cv::threshold(gray, thre1, value1, 255, cv::THRESH_BINARY);
+			cv::threshold(gray, thre2, value2, 255, cv::THRESH_BINARY);
 		}
 		else if (mode == 1) {
-			cv::threshold(gray, thre, 0, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
-		}
-		else if (mode == 2) {
-			cv::threshold(gray, thre, value, 255, cv::THRESH_BINARY);
-		}
-		else if (mode == 3) {
-			cv::threshold(gray, thre, value, 255, cv::THRESH_BINARY_INV);
+			cv::threshold(gray, thre1, value1, 255, cv::THRESH_BINARY_INV);
+			cv::threshold(gray, thre2, value2, 255, cv::THRESH_BINARY_INV);
 		}
 
+		cv::bitwise_xor(thre1,thre2,thre0);
+
 		//表示
-		cv::imshow("thre",thre);
+		cv::imshow("thre1",thre1);
+		cv::imshow("thre2",thre2);
+		cv::imshow("thre0",thre0);
 
 		//輪郭の座標リスト
 		std::vector<std::vector<cv::Point>> contours;
 		//輪郭の取得
-		cv::findContours(thre, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+		cv::findContours(thre0, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 		//検出された輪郭線を緑で描画
-		cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0));
+		//cv::drawContours(frame, contours, -1, cv::Scalar(0, 255, 0));
 
 		//直線近似
-		std::vector<std::vector<cv::Point>> approxes;
+		problem.pieces.erase(problem.pieces.begin(),problem.pieces.end());
 		for(auto contour = contours.begin(); contour != contours.end(); contour++){
 			std::vector<cv::Point> approx;
+			Polygon polygon;
 
 			//輪郭を直線近似する
-			cv::approxPolyDP(cv::Mat(*contour), approx, 3.5, true);
-			approxes.push_back(approx);
+			cv::approxPolyDP(cv::Mat(*contour), approx, 3.0, true);
+			for(auto ite = approx.begin(); ite != approx.end(); ite++){
+				Point v;
+				cvPointToPoint(*ite, v);
+				polygon.addNode(v);
+			}
+			if(polygon.size()>=3){
+				problem.pieces.push_back(polygon);
+				//近似した輪郭線を青で描画
+				cv::polylines(frame, approx, true, cv::Scalar(255, 0, 0));
+			}
+			
 		}
-		drawContours(frame, approxes, -1, cv::Scalar(255, 0, 0));
 
 
 		//キー取得
 		int key = cv::waitKey(1);
 		
-		if(CheckHitKey(key,'q'))
-		{
+		if(CheckHitKey(key,'q')){
 			//whileループから抜ける．
 			break;
+		}else if(CheckHitKey(key, 'p')){
+			pause = !pause;
 		}
-		else if(CheckHitKey(key,'s') && nutoral)
-		{
-			//フレーム画像を保存する．
-			std::string filename;
+		else if(CheckHitKey(key,'s') && nutoral){
+			//ピースの頂点情報の出力.
+			std::ofstream ofs("polygon_out.txt");
+			for(int i=0;i<problem.pieces.size();i++){
+				ofs << problem.pieces[i] << std::endl;
+			}
+			ofs.close();
 
+			//フレーム画像を保存する.
+			std::string filename;
 			Console::SetCursorPos(0,15);
 			std::cout << "ファイル名入力";
 			std::cin  >> filename;
 
 
 			if(filename + ".png" != image_file){
-				cv::Mat out;
-				if     (mode == 1)out = thre;
-				else if(mode == 2)out = gray;
-				else              out = frame;
-				cv::imwrite(filename + ".png", out);
+				cv::imwrite(filename + ".png", frame);
 				std::cout << "セーブ完了[" << filename << ".png]" << std::endl;
 			}else{
 				std::cout << "上書きは勘弁して" << std::endl;
@@ -134,13 +159,19 @@ int main(){
 		}else if (CheckHitKey(key,'t')){
 			//二値化
 			mode++;
-			mode %= 4;
+			mode %= 2;
 		}else if(CheckHitKey(key,'[')){
 			//増加
-			value+=5;
+			value1+=1;
 		}else if(CheckHitKey(key,']')){
 			//減少
-			value-=5;
+			value1-=1;
+		}else if(CheckHitKey(key,'@')){
+			//増加
+			value2+=1;
+		}else if(CheckHitKey(key,':')){
+			//減少
+			value2-=1;
 		}
 		//キー解放
 		if(key == -1){
@@ -150,17 +181,24 @@ int main(){
 		}
 
 		//描画
-		Console::SetCursorPos(10,8);
-		std::cout << mode << "   ";
 		Console::SetCursorPos(10,9);
-		std::cout << value << "   ";
+		std::cout << mode << "   ";
 		Console::SetCursorPos(10,10);
+		std::cout << value1 << "   ";
+		Console::SetCursorPos(10,11);
+		std::cout << value2 << "   ";
+		Console::SetCursorPos(10,12);
 		std::cout << key << "      ";
-		Console::SetCursorPos(0,20);
+		Console::SetCursorPos(10, 13);
+		std::cout << problem.pieces.size() <<"   ";
+		Console::SetCursorPos(0,23);
+
 
 		std::cout << std::flush;
 
 		cv::imshow("window", frame);//画像を表示．
+
+		//cv::waitKey(0);
 	}
 	cv::destroyAllWindows();
 	return 0;
@@ -171,3 +209,9 @@ bool CheckHitKey(int key,char c){
 	//return key == (c-'a' + 97);
 	return key == (c-'a' + 1048673);
 }
+
+void cvPointToPoint(cv::Point &cvpoint, Point &point){
+	point.x = cvpoint.x;
+	point.y = cvpoint.y;
+}
+
