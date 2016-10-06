aa040ae10e4f13b0e2de99cda2922ec14170e505
Hal Stroemeria <fripfrop@docomo.ne.jp>
2016-10-07 00:58:59 +0900

    Deproyment::Solverをある程度記述

--- solver/modules/search/include/search/Deproyment.h
+++ solver/modules/search/include/search/Deproyment.h
@@ -17,9 +17,14 @@ public:
 		double angle_sum[2]; //角度和
 	};
 private:
+	constexpr int LENGTH_EPS = 1.0e1;
+	constexpr int ANGLE_EPS  = 1.0e1;
 	Polygon base;//ベース
 
-	std::vector<result> Solver();
+	double Length(const Polygon& p,int index);
+	double Angle (const Polygon& p,int index,bool Rev=false);
+
+	std::vector<result> Solver(std::vector<Polygon> list);
 
 public:
 
--- solver/modules/search/src/Deproyment.cpp
+++ solver/modules/search/src/Deproyment.cpp
@@ -1,13 +1,62 @@
 
 #include <Deproyment.h>
 
+double Deproyment::Length(const Polygon& p,int index){
+	int index_n = (index+1) % p.size();
+	return (p.getNode(index) - p.getNode(index_n)).size();
+}
+double Deproyment::Angle (const Polygon& p,int index,bool Rev){
+	int j_p = (index+p.size()-1) % p.size();
+	int j_n = (index+1         ) % p.size();
+	if(Rev)return Point::getAngle2Vec(p.getNode(j_p),p.getNode(j_n));
+	else   return Point::getAngle2Vec(p.getNode(j_n),p.getNode(j_p));
+}
+
+std::vector<result> Deproyment::Solver(std::vector<Polygon> list){
+	std::vector<result>	 ans;
+	//長さ角度が規定値以下
+
+	//全て正規化
+	for(Polygon& p:list) p.normalize();
+
+	for(int k = 0;k<base.size();k++){
+		double base_length = Length(base,k);
+		double base_angle  = Angle (base,k,true);
+
+		for(int i=0;i<list.size();i++){
+			for(int j=0;j<list[i].size();j++){
+				double length = Length(list[i],j);
+				double angle  = Angle (list[i],j);
+				
+				//規定値以下
+				if(std::abs(base_length - length) < LENGTH_DIFF ||
+				   std::abs(base_angle + angle - M_PI * (1/2) ) < ANGLE_EPS ||
+				   std::abs(base_angle + angle - M_PI * (2/2) ) < ANGLE_EPS ||
+				   std::abs(base_angle + angle - M_PI * (3/2) ) < ANGLE_EPS ||
+				   std::abs(base_angle + angle - M_PI * (4/2) ) < ANGLE_EPS){
+					TransParam param;
+					param.sub_index = j;
+					param.pos       = base.getNode(k);
+					param.reverse   = false;
+					param.angle     = Point::getAngle2Vec(base    .getNode((k+1)%base    .size())-base.getNode(k),
+					                                      frame[i].getNode((j+1)%frame[i].size())-frame[i].getNode(j));
+				}
+			}
+		}
+	}
+
+	return ans;
+}
 
 
-void Deproyment::Run(){
+
+void Deproyment::Run(std::vector<Polygon> list){
 
 	//ベースを中心に展開、キーで候補入れ替え
 	std::cout << "==================== DEPROYMENT ====================" << std::endl;
+	std::vector<result> res = Solver(list);
 	while(1){
+
 		
 		
 		
